7e17e4d80c7df5c21280c8998483659a130c6969
abdullahN111 <162550451+abdullahN111@users.noreply.github.com>
2024-04-13 15:23:58 +0500

    I have added code to execute my Currency Converter

---
+++ main.ts
@@ -0,0 +1,44 @@
+import inquirer from "inquirer";
+
+let currency: any = {
+  USD: 1, //base currency
+  EUR: 0.92,
+  PKR: 278,
+  INR: 83.30,
+  BTK: 109.50,
+  AUD: 1.52,
+  TRY: 32.08,
+  CNY: 7.23
+};
+
+let userAnswer: any = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Enter FROM currency",
+    type: "list",
+    choices: ["USD", "EUR", "PKR", "INR","BTK","AUD","TRY","CNY"]
+  },
+  {
+    name: "to",
+    message: "Enter TO currency",
+    type: "list",
+    choices: ["USD", "EUR", "PKR", "INR","BTK","AUD","TRY","CNY"]
+  },
+  {
+    name: "amount",
+    message: "Enter your amount",
+    type: "number"
+  },
+
+]);
+
+let fromAmount = currency[userAnswer.from]
+let toAmount = currency[userAnswer.to]
+let amount = userAnswer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+
+console.log(convertedAmount);
+// console.log(fromAmount);
+// console.log(toAmount);
+// console.log(amount);
